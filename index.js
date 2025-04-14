474be634d630b168cecf2d86192072df38c9d7c6
willbert978 <wcameron2003@gmail.com>
2025-04-13 19:35:34 -0500

    I got the listner fucntion to display numbers when clicked and store the first number and operator after they are clicked.

--- index.js
+++ index.js
@@ -1,43 +1,73 @@
-var number1 = '';
-var number2 = '';
-var firstOperator = null;
-var secondOperator = null;
-let result = null;
-const displayValue = 0;
-document.getElementById('display');
-
-function add (number1, number2) {
-    return number1 + number2
-}
-function subtract (number1, number2) {
-    return number1 - number2
-}
-function multiply (number1, number2) {
-    return number1 * number2
-}
-function divide (number1, number2) {
-    return number1 / number2
-}
-
-function operate (operator, number1, number2) {
-   switch (operator) {
-    case '+':
-    return add(number1, number2);
-    case '-':
-    return subtract(number1, number2);
-    case '*':
-    return multiply(number1, number2);
-    case '/':
-    return divide(number1, number2);  
-   }
+document.addEventListener('DOMContentLoaded', function() {
+    var number1 = '';
+    var number2 = '';
+    var firstOperator = null;
+    var secondOperator = null;
+    let result = null;
+    let displayValue = 0;
+    let display = document.getElementById('display');
     
+    
+    function add (number1, number2) {
+        return number1 + number2
+    }
+    function subtract (number1, number2) {
+        return number1 - number2
+    }
+    function multiply (number1, number2) {
+        return number1 * number2
+    }
+    function divide (number1, number2) {
+        return number1 / number2
+    }
+    
+    function operate (operator, number1, number2) {
+       switch (operator) {
+        case '+':
+        return add(number1, number2);
+        case '-':
+        return subtract(number1, number2);
+        case '*':
+        return multiply(number1, number2);
+        case '/':
+        return divide(number1, number2);  
+       }
+        
+    
+    }
+    
+    function appendToDisplay (input) {
+        display.value += input
+      
+    }
+    
+    function clearDisplay () {
+        display.value = '';
+    }
+    
+    function listener () {
+    
+    const input = document.querySelector("#keys");
+    const operator = document.querySelectorAll(".operator-btn");
+    const numberButtons = document.querySelectorAll(".number-btn");
+    
+    numberButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            appendToDisplay(event.target.textContent);
+        });
+    });
 
-}
+    for (let i = 0; i < operator.length; i++) {
+        operator[i].addEventListener("click", function(event) {
+            firstOperator = event.target.textContent;
+            number1 = display.value;
+            appendToDisplay(firstOperator);
+        });
+    }
+    
+    }
+    listener();
+    
+  });
+  
 
-function appendToDisplay (input) {
-    display.value += input
-}
-
-function clearDisplay () {
-    display.value = '';
-}
