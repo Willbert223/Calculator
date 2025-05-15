4b46eb498a8e2eb19dedfa064358a07e037034e2
willbert978 <wcameron2003@gmail.com>
2025-05-15 00:40:00 -0500

    got the very first step to work all by myself ZERO CHATGPT TO DO ANY OF IT

---
+++ index.js
@@ -0,0 +1,23 @@
+// store the first num, operator, second num
+let firstnum = '';
+let operator = '';
+let secondnum = '';
+
+// add the first num, and second num
+function add(firstnum, secondnum) {
+  return firstnum + secondnum;
+}
+// subtract first num, second num
+function subtract(firstnum, secondnum) {
+  return firstnum - secondnum;
+}
+// multiply first and second num
+function multiply(firstnum, secondnum) {
+  return firstnum * secondnum;
+}
+// divide first and second num.
+function divide(firstnum, secondnum) {
+  return firstnum / secondnum;
+}
+
+console.log(multiply(2,3))
