ab418e2f1781742200329b8ea25b3ec8f7287799
willbert978 <wcameron2003@gmail.com>
2025-05-16 01:32:11 -0500

    Got the operate function to work by googling the code used and turning my variables into integers to call operate the psuedocode is mine

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// store the first num, operator, second num
-let firstnum = '';
-let operator = '';
-let secondnum = '';
+
 
 // add the first num, and second num
 function add(firstnum, secondnum) {
@@ -20,4 +17,28 @@ function divide(firstnum, secondnum) {
   return firstnum / secondnum;
 }
 
-console.log(multiply(2,3))
+// operate on two stored numbers depending on operator I click
+function operate (firstnum, secondnum, operator) {
+// if operator is + return add function
+ switch (operator) {
+  case '+':
+    return add(firstnum, secondnum)
+// if operator is - return subtract function
+  case '-':
+    return subtract(firstnum, secondnum)
+// if operator is * return multiply function
+  case '*':
+    return multiply(firstnum, secondnum)
+// if operator is / return divide function
+  case '/':
+    return divide(firstnum, secondnum)    
+ }
+ 
+}
+
+// store the first num, operator, second num
+let firstnum = parseInt('3');// converts string to integer
+let operator = '/';
+let secondnum = parseInt('3');
+
+console.log(operate(firstnum, secondnum, operator))
